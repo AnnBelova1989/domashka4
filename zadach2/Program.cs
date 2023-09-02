79c4b367e0ecef06c40fbbbc3cc1e545d6761982
AnnBelova <anutik-89@tut.by>
2023-09-03 00:38:52 +0300

    задачи домашнее

--- zadach2/Program.cs
+++ zadach2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNumber(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+int Sum(int num)
+{
+  int sum=0;
+    while (num>0)
+    {
+        sum+=num%10;
+        num=num/10;
+    }
+   /* {
+        for ( i = 0; num!=0; i++,num/=10)
+        */
+    return sum;
+}
+int number=InputNumber("Введите число: ");
+int result=Sum(number);
+System.Console.WriteLine($"Сумма цифр заданного числа= {result} ");
--- zadach3/Program.cs
+++ zadach3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNumber(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+int[] ArrayofNumbers(int size)
+{
+    int[]array= new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i]=new Random().Next();
+    }
+    return array;
+}
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"{array[i]}, ");
+}
+}
+    int size=InputNumber("Введите число элементов: ");
+    int[]result= ArrayofNumbers(size);
+    PrintArray (result);
