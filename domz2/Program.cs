93ad8b7b5da902a137cd402c95fe06a71cc2ead8
AnnBelova <anutik-89@tut.by>
2023-09-08 21:48:34 +0300

    2

--- domz2/Program.cs
+++ domz2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задать одномерный массив , заполненный случайными числами. Найдите сумму элементов, состоящих на нечетных позициях.
+
+int Input(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[] Array(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 10);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+void SumElem(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    System.Console.WriteLine($"Сумма чисел на нечетных позициях равна {sum}");
+}
+
+int size = Input("Введите размер массива: ");
+int[] array = Array(size);
+FillArray(array);
+PrintArray(array);
+SumElem(array);
