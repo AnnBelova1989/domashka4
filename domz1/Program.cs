0c2784b5f38c7aea8dd572ee07fa0ed6dc240df3
AnnBelova <anutik-89@tut.by>
2023-09-08 21:47:58 +0300

    1

--- domz1/Program.cs
+++ domz1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив, заполненнный случайными положительными трехзначными числами. Напишипте программу, к-я покажет кол-во четных чисел в массиве
+
+int Input(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(100, 1000);
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+int Chetn(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i ++)
+    {
+        if(arr[i]%2==0)
+        count++;
+    }
+    return count;
+}
+
+int size = Input("Введите размер массива: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray);
+PrintArray(myArray);
+int chetn = Chetn(myArray);
+System.Console.WriteLine($"Количество четных чисел в массиве {chetn}");
